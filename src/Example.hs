c6a782e63017668f554078632630377305527be3
minifig404 <proegssilb@gmail.com>
2015-12-17 09:19:39 -0500

    WIP: Link to Project from Project List

--- src/Example.hs
+++ src/Example.hs
@@ -7,23 +7,25 @@ import           BeholderObserver.Constant
 import           BeholderObserver.Data
 import           Data.Foldable (asum)
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as HA
 import qualified Web.Scotty as S
 import qualified Text.Blaze.Html.Renderer.Text as TR
-
-renderProjectListItem :: Project -> H.Html
-renderProjectListItem = H.li . H.toHtml . projName
+import qualified Data.Text as T
 
 renderProject :: Project -> H.Html
 renderProject = H.html . H.body . H.h1 . H.toHtml . projName
 
+renderProjectList :: DataLoader d => d -> H.Html
+renderProjectList dl = H.html $ H.body $ do
+  H.h1 "What are you working on today?"
+  H.ul . mapM_ drawItem . listProjects $ dl
+  where drawItem p = H.li . (H.a  H.! url p ). H.toHtml . projName $ p
+        url p = HA.href . T.pack $ "/proj/" ++ projId p
+
 site :: ConstantDataLoader -> S.ScottyM ()
 site dl = do
   S.get "/" $
-    S.html . TR.renderHtml $
-      H.html $
-        H.body $ do
-          H.h1 "What are you working on today?"
-          H.ul . mapM_ renderProjectListItem . listProjects $ dl
+    S.html . TR.renderHtml . renderProjectList $ dl
   S.get "/test" $ S.text "Hello World!"
   S.get "/proj/:projId" $ do
     pid <- S.param "projId"
