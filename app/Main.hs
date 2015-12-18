e5d0a3173ee9b28736a6ac126541cd42914921ec
David Bliss <minifig404@gmail.com>
2015-12-18 15:38:02 -0500

    Merging migration to scotty/web app style; ability to list doc sets in a project.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import           Example (runApp)
 
 main :: IO ()
-main = someFunc
+main = runApp
---
+++ src/BeholderObserver/Constant.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module BeholderObserver.Constant (
+ConstantDataLoader(..)
+) where
+
+import BeholderObserver.Data
+
+data ConstantDataLoader = ConstantDataLoader
+
+-------------------------------------
+-- Data we're exposing. See below for typeclass
+-- implementation.
+
+projects = [
+              Project "infinity-ide" "Infinity IDE" [
+                DocSet "llvm" "LLVM" [],
+                DocSet "titandb" "TitanDB" [],
+                DocSet "neo4j" "Neo4J" [],
+                DocSet "sql" "SQL" [],
+                DocSet "compilers" "Compilers" [],
+                DocSet "refactoring" "Refactoring" [],
+                DocSet "formal-methods" "Formal Methods" [],
+                DocSet "graph-algorithms" "Graph Algorithms" [],
+                DocSet "comparison-of-ides" "Comparison of IDEs" [],
+                DocSet "specification" "Specification" [],
+                DocSet "version-control" "Version Control" []
+              ],
+              Project "genealogy" "Genealogy" [],
+              Project "vr-os" "Virtual Reality OS" [
+                DocSet "vr-headsets" "VR Headsets" [],
+                DocSet "process-management" "Process Management" [],
+                DocSet "processor-boot-sequences" "Processor Boot Sequences" [],
+                DocSet "kernel-design" "Kernel Design Analysis" [],
+                DocSet "filesystems" "Filesystems" [],
+                DocSet "hw-sw-interface" "Harware/Software Interface" []
+              ],
+              Project "weary-examiner" "Weary Examiner" [
+                DocSet "riak" "Riak" [],
+                DocSet "sql" "SQL" [],
+                DocSet "table-reqs" "Table Requirements" [],
+                DocSet "server-reqs" "Server Requirements" [],
+                DocSet "docker" "Docker" [],
+                DocSet "radio-tech" "Radio Tech" [],
+                DocSet "ui-mockups" "UI Mockups" []
+              ],
+              Project "website" "Website" [],
+              Project "kanban-boards" "Kanban Boards" [],
+              Project "pipelang" "PipeLang" [],
+              Project "bi-system" "BI System" [],
+              Project "ddos-web-server" "DDoS-resistant Web Server" []
+            ]
+
+
+-------------------------------------
+-- Typeclass implementation.
+instance DataLoader ConstantDataLoader where
+  listProjects dl = projects
+  readDocSets dl = projDocSets
+  readDocs dl = dsDocs
+  findDoc dl _ = []
